9ed9e2659c256b759e23bca38e0fbdc1f8158017
laia <laia.igelmo.a@gmail.com>
2023-02-25 23:12:16 +0100

    -Add loop to introduce more than one mushroom to evaluate -Generate decisional tree inside each case 1 or 2, in order to avoid the generation of a decisional tree without a test mushroom specified. -Add option to end loop (input 3).

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -3,6 +3,7 @@ import domain.MushroomMap.generateRandomMushroomToTest
 import domain.{MushroomMap, generateDecisionalTree}
 
 import scala.io.StdIn
+import scala.util.control.Breaks.break
 object main {
 
   def main(args: Array[String]) = {
@@ -14,30 +15,32 @@ object main {
     ingestSQLQuery.showMushroomsDataFrame
     ingestSQLQuery.showDifferentCapColors
 
-    println("Do you want to predict a random mushroom or a specific one? \n" +
-      "random: 1, specified: 2")
-    val option = StdIn.readLine()
-    var mushroomToEvaluate: Array[Int] = Array.emptyIntArray
+    while(true) {
+      println("Do you want to predict a random mushroom or a specific one? \n" +
+        "random: 1, specified: 2, exit: 3")
+      val option = StdIn.readLine()
+      var mushroomToEvaluate: Array[Int] = Array.emptyIntArray
 
-    option match {
-      case "1" => {
-        println("Generating random mushroom...")
-        mushroomToEvaluate = generateRandomMushroomToTest
-        println()
-        println(mushroomToEvaluate.mkString("Array(", ", ", ")"))
+      option match {
+        case "1" =>
+          println("Generating random mushroom...")
+          mushroomToEvaluate = generateRandomMushroomToTest
+          println()
+          println(mushroomToEvaluate.mkString("Array(", ", ", ")"))
+          decisionalTreeInstance.generateDecisionalTree(mushroomToEvaluate)
+
+        case "2" =>
+          println("Please, enter your mushroom characteristics to predict if it's edible or not:")
+          val newMushroom = mushroomMap.introduceMushroom()
+          println("You have introduced the next mushroom:")
+          println(newMushroom.mkString("Array(", ", ", ")"))
+          mushroomToEvaluate = mushroomMap.convertMushroomToMappedIntMushroom(newMushroom)
+          decisionalTreeInstance.generateDecisionalTree(mushroomToEvaluate)
+
+        case "3" => break
+        case _ => println("that's not an option")
       }
-      case "2" => {
-        println("Please, enter your mushroom characteristics to predict if it's edible or not:")
-        val newMushroom = mushroomMap.introduceMushroom()
-        println("You have introduced the next mushroom:")
-        println(newMushroom.mkString("Array(", ", ", ")"))
-        mushroomToEvaluate = mushroomMap.convertMushroomToMappedIntMushroom(newMushroom)
-      }
-      case _ => println("that's not an option")
     }
-
-    decisionalTreeInstance.generateDecisionalTree(mushroomToEvaluate)
-
   }
 
 
